31dbcda669fdb19e3d0e617c128ed3e0982969c4
Michael <mren@mycompany.com>
2016-11-16 11:21:37 -0500

    Initial project

---
+++ proxy/server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.get('/proxy.pac', function (req, res) {
+  fs.readFile('./proxy.pac', function (err, pacfile) {
+		if (err) {
+			console.log(err);
+		}
+		res.writeHeader(200, {"Content-Type" : "application/x-ns-proxy-autoconfig"});
+		res.write(pacfile);
+		res.end();
+		console.log("PAC file sent...");
+	});
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+})
